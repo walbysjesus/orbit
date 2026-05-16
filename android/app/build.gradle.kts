84694e669954f24947c05c56735dc5d726c8ca9b
Walbys Rodriguez <57323396+walbysjesus@users.noreply.github.com>
2026-05-15 21:36:46 -0500

    fix: remove ndk abiFilters conflict with abi splits

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -125,15 +125,6 @@ android {
         }
     }
 
-    splits {
-        abi {
-            isEnable = true
-            reset()
-            include("armeabi-v7a", "arm64-v8a", "x86_64")
-            isUniversalApk = false
-        }
-    }
-
     lint {
         checkReleaseBuilds = !lowMemoryBuild
         abortOnError = !lowMemoryBuild
